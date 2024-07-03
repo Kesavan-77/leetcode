32a40fc1c0fdd0bd9adf94fda22e9fd33c659ff4
Kesavan V <117655114+Kesavan-77@users.noreply.github.com>
2024-07-03 22:07:49 +0530

    Time: 105 ms (85.42%), Space: 58.6 MB (27.08%) - LeetHub

---
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minDifference = function (nums) {
+    const n = nums.length;
+    if (n < 5) return 0;
+    nums.sort((a, b) => a - b);
+    const minDiffs = [
+        nums[n - 1] - nums[3],  
+        nums[n - 2] - nums[2],  
+        nums[n - 3] - nums[1],
+        nums[n - 4] - nums[0]
+    ];
+
+    return Math.min(...minDiffs);
+};
