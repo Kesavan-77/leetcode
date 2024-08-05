e282c8940f163d25ebc55f1b50e8faa886b2796f
Kesavan V <117655114+Kesavan-77@users.noreply.github.com>
2024-08-05 21:45:29 +0530

    Time: 59 ms (95.89%), Space: 17 MB (7.74%) - LeetHub

--- 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.py
+++ 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.py
@@ -1,15 +1,23 @@
 class Solution:
     def kthDistinct(self, arr: List[str], k: int) -> str:
-        map = {}
-        for word in arr:
-            if word in map:
-                map[word] += 1
-            else:
-                map[word] = 1
-        print(map)
+        from collections import Counter
+        map = Counter(arr)
         for key in map.keys():
             if(map[key]==1):
                 if(k==1):
                     return key
                 k-=1
         return ""
+    
+        # map = {}
+        # for word in arr:
+        #     if word in map:
+        #         map[word] += 1
+        #     else:
+        #         map[word] = 1
+        # for key in map.keys():
+        #     if(map[key]==1):
+        #         if(k==1):
+        #             return key
+        #         k-=1
+        # return ""
