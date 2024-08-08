3d9f949e3b686592f54f9237568986419271d626
Kesavan V <117655114+Kesavan-77@users.noreply.github.com>
2024-08-08 23:30:02 +0530

    Time: 68 ms (97.66%), Space: 17.9 MB (14.45%) - LeetHub

--- 0885-spiral-matrix-iii/0885-spiral-matrix-iii.py
+++ 0885-spiral-matrix-iii/0885-spiral-matrix-iii.py
@@ -1,40 +1,44 @@
 from typing import List
+
 class Solution:
     def spiralMatrixIII(self, rows: int, cols: int, rStart: int, cStart: int) -> List[List[int]]:
-        lptr, rptr = cStart, cStart
-        topptr, bottomptr = rStart, rStart
-        ans = [[0, 0] for _ in range(rows * cols)]
-        index = 0
-        ans[index][0] = rStart
-        ans[index][1] = cStart
-        index += 1
+        leftPointer, rightPointer = cStart, cStart
+        topPointer, bottomPointer = rStart, rStart
+        res = []
+        res.append([rStart, cStart])
+        index = 1
         while index < (rows * cols):
-            # right
-            rptr += 1
-            for i in range(lptr + 1, rptr + 1):
-                if topptr >= 0 and i >= 0 and i < cols:
-                    ans[index][0] = topptr
-                    ans[index][1] = i
+
+            rightPointer += 1
+            i = leftPointer + 1
+            while i <= rightPointer:
+                if topPointer >= 0 and i >= 0 and i < cols:
+                    res.append([topPointer, i])
                     index += 1
-            # bottom
-            bottomptr += 1
-            for i in range(topptr + 1, bottomptr + 1):
-                if rptr < cols and i >= 0 and i < rows:
-                    ans[index][0] = i
-                    ans[index][1] = rptr
+                i += 1
+
+            bottomPointer += 1
+            i = topPointer + 1
+            while i <= bottomPointer:
+                if rightPointer < cols and i >= 0 and i < rows:
+                    res.append([i, rightPointer])
                     index += 1
-            # left
-            lptr -= 1
-            for i in range(rptr - 1, lptr - 1, -1):
-                if bottomptr < rows and i >= 0 and i < cols:
-                    ans[index][0] = bottomptr
-                    ans[index][1] = i
+                i += 1
+
+            leftPointer -= 1
+            i = rightPointer - 1
+            while i >= leftPointer:
+                if bottomPointer < rows and i >= 0 and i < cols:
+                    res.append([bottomPointer, i])
                     index += 1
-            # top
-            topptr -= 1
-            for i in range(bottomptr - 1, topptr - 1, -1):
-                if lptr >= 0 and i >= 0 and i < rows:
-                    ans[index][0] = i
-                    ans[index][1] = lptr
+                i -= 1
+
+            topPointer -= 1
+            i = bottomPointer - 1
+            while i >= topPointer:
+                if leftPointer >= 0 and i >= 0 and i < rows:
+                    res.append([i, leftPointer])
                     index += 1
-        return ans
+                i -= 1
+
+        return res
