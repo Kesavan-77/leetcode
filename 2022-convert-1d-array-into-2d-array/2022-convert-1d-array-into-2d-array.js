eb8fc0793f3b23ccfaf91fd02d1211e64c01d47b
Kesavan V <117655114+Kesavan-77@users.noreply.github.com>
2024-09-01 15:13:47 +0530

    Time: 166 ms (89.34%), Space: 69.3 MB (78.17%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.js
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.js
@@ -4,18 +4,29 @@
  * @param {number} n
  * @return {number[][]}
  */
+
 var construct2DArray = function(original, m, n) {
     let res = [];
-    if(m*n !== original.length) return res;
-    let start = 0;
-    while(m>0 && original.length){
-        let temp = [],col=0;
-        while(col<n && start<original.length){
-            temp.push(original[start++]);
-            col++;
-        }
-        res.push(temp);
-        m--;
+    if (original.length !== m * n) return res;
+    for (let i = 0; i < m; i++) {
+        res.push(original.slice(i * n, i * n + n));
     }
+    
     return res;
 };
+
+// var construct2DArray = function(original, m, n) {
+//     let res = [];
+//     if(m*n !== original.length) return res;
+//     let start = 0;
+//     while(m>0 && original.length){
+//         let temp = [],col=0;
+//         while(col<n && start<original.length){
+//             temp.push(original[start++]);
+//             col++;
+//         }
+//         res.push(temp);
+//         m--;
+//     }
+//     return res;
+// };
