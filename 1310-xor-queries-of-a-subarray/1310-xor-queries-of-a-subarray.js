8cf8340a339bd1c914d74a122678072f85f21f3d
Kesavan V <117655114+Kesavan-77@users.noreply.github.com>
2024-09-13 22:59:07 +0530

    Time: 83 ms (87.18%), Space: 63.7 MB (51.28%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.js
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.js
@@ -4,13 +4,19 @@
  * @return {number[]}
  */
 var xorQueries = function(arr, queries) {
+    let values = [];
+    let cal = 0;
+    for(let i=0;i<arr.length;i++){
+        cal ^= arr[i];
+        values.push(cal)
+    }
     let res = [];
-    for(let val of queries) {
-        let cal = 0;
-        for(let i=val[0];i<=val[1];i++){
-            cal ^= arr[i];
+    for(let val of queries){
+        if(val[0]-1>=0){
+            res.push(values[val[1]] ^ values[val[0]-1]);
+        } else {
+            res.push(values[val[1]]);
         }
-        res.push(cal);
     }
     return res;
 };
