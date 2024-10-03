37a680cb332e9c9d66d6531b2dfd523fd9663a6d
Kesavan V <117655114+Kesavan-77@users.noreply.github.com>
2024-10-03 20:03:15 +0530

    Time: 25 ms (66.67%), Space: 57 MB (19.72%) - LeetHub

--- 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.java
+++ 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.java
@@ -1,14 +1,13 @@
 class Solution {
-    public long sumArray(int[] arr){
+    public long sumArray(int[] arr) {
         long total = 0;
-        for(int val : arr){
+        for (int val : arr) {
             total += val;
         }
         return total;
     }
     public int minSubarray(int[] nums, int p) {
         long remainder = sumArray(nums) % p;
-        System.out.println(sumArray(nums) + " " + remainder);
         if (remainder == 0) return 0;
         int res = nums.length;
         HashMap<Long, Integer> map = new HashMap<>();
@@ -16,10 +15,9 @@ class Solution {
         long curr_sum = 0;
         for (int i = 0; i < nums.length; i++) {
             curr_sum = (curr_sum + nums[i]) % p;
-            long prefix = (curr_sum - remainder + p) % p;
+            long prefix = (curr_sum - remainder + p) % p; 
             if (map.containsKey(prefix)) {
-                long length = i - map.get(prefix);
-                res = Math.min(res, (int) length); 
+                res = Math.min(res, i - map.get(prefix));
             }
             map.put(curr_sum, i);
         }
