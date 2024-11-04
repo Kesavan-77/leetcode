eadd6be33fba1d5d451f62dcba859155e709bc74
Kesavan V <117655114+Kesavan-77@users.noreply.github.com>
2024-11-04 10:51:09 +0530

    Time: 1005 ms (10.17%), Space: 51.4 MB (16.25%) - LeetHub

--- 3163-string-compression-iii/3163-string-compression-iii.java
+++ 3163-string-compression-iii/3163-string-compression-iii.java
@@ -1,21 +1,22 @@
 class Solution {
     public String compressedString(String word) {
-        StringBuilder comp = new StringBuilder();
+        String comp = "";
         int count = 1;
-        for (int i = 0; i < word.length() - 1; i++) {
+        int n = word.length();
+        for (int i = 0; i < n-1; i++) {
             if (word.charAt(i) == word.charAt(i + 1)) {
                 count++;
-                if (count == 9 && i+1!=word.length()-1) {
-                    comp.append(count).append(word.charAt(i));
+                if (count == 9 && i + 1 != n - 1) {
+                    comp += count + "" + word.charAt(i);
                     count = 1;
                     i++;
                 }
             } else {
-                comp.append(count).append(word.charAt(i));
+                comp += count + "" + word.charAt(i);
                 count = 1;
             }
         }
-        comp.append(count).append(word.charAt(word.length() - 1));
-        return comp.toString();
+        comp += count + "" + word.charAt(n - 1);
+        return comp;
     }
 }
